af7bc910ebd74346856a7ace8e7479fbc17739cd
hamed-bavar <54584195+hamed-bavar@users.noreply.github.com>
2022-05-12 00:30:36 +0430

    [Add] test

---
+++ src/pages/About.test.js
@@ -0,0 +1,46 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import About from '../pages/About';
+describe("AddInput", () => {
+    it('should render input element', () => {
+        render(
+            <About
+            />
+        );
+
+        expect(true).toBe(true);
+    });
+    it('should render input element', () => {
+        render(
+            <About
+            />
+        );
+        expect(true).toBe(true);
+    });
+
+    it('should render input element', () => {
+        render(
+            <About
+            />
+        );
+        expect(true).toBe(true);
+    });
+
+
+    it('should render input element', () => {
+        render(
+            <About
+            />
+        );
+        expect(true).toBe(true);
+    });
+
+    it('should render input element', () => {
+        render(
+            <About
+            />
+        );
+        expect(true).toBe(true);
+    });
+
+
+})
