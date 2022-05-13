bec97b8f77e2caaf2c55072ae69c14c0f2c6e8f2
0Some1 <50609516+0Some1@users.noreply.github.com>
2022-05-13 20:54:29 +0430

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ function About(props) {
     return (
         <div>
             <h1>About</h1>
-            <p>This is the tic tac to app v1.0.2 and It is for the Internet Engineer course.</p>
+            <p>This is the tic tac to app v1.0.1 and It is for the Internet Engineer course.</p>
             <p>M.Sadegh Gh</p>
         </div>
     );
